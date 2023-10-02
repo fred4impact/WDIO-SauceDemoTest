1d3ae2e883c12d6e299f87c7cee11ec84a274a25
fred4impact <runtesting4me@gmail.com>
2023-10-02 11:22:09 +0100

    fist commit

---
+++ test/pageobjects/products.page.js
@@ -0,0 +1,12 @@
+const { $ } = require('@wdio/globals')
+const Page = require('./page');
+
+
+class Products extends Page{
+
+    /**
+     * define selectors using getter methods
+     */
+}
+
+module.exports = new Products();
